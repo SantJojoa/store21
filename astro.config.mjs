fa0f83cd54aa15d48c6e164bcfe159584f91c337
Santiago Jojoa <santiago.jojoan@gmail.com>
2026-07-16 12:07:45 -0500

    dep: Deploy to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
